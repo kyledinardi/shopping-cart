072d8ad5816f37a661659053b0eb53ffae88406d
Kdin <94251418+awesome13524@users.noreply.github.com>
2024-04-02 13:35:23 -0400

    Style homepage and add more styles to Navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,9 @@
+import styles from '../style/Home.module.css'
+
 export default function Home() {
   return (
-    <div>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam magna
-      justo, pulvinar ac lectus eget, pretium vulputate libero. Ut lacinia massa
-      id mauris varius sodales. Vestibulum in feugiat magna. Morbi mauris magna,
-      semper non diam condimentum, tempor tristique mauris. Nunc diam ex, congue
-      a ex at, posuere mollis augue. Etiam faucibus odio odio, sit amet
-      venenatis est tempus maximus. Aliquam sit amet pulvinar magna. Suspendisse
-      volutpat erat quis libero sollicitudin, non ultricies lorem iaculis. Fusce
-      porta velit vel arcu sollicitudin, eget varius dui pulvinar. Orci varius
-      natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-      Pellentesque et mi egestas, rhoncus augue eu, suscipit leo. Morbi
-      venenatis nulla sit amet facilisis finibus. Proin ullamcorper volutpat
-      ipsum nec rutrum. Nam consectetur, leo ac ultricies pulvinar, elit velit
-      imperdiet nunc, vitae accumsan neque ex a metus. Integer id velit ante.
+    <div className={styles.home}>
+      <h1>Welcome to Shopamajig, the premiere ecommerce site!</h1>
     </div>
   );
 }
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,3 +1,5 @@
+import styles from '../style/Shop.module.css';
+
 export default function Shop() {
   return (
     <div>

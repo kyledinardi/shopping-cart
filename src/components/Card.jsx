30659def7ae1ee721a247526e4b7079f3fd4d577
Kdin <94251418+awesome13524@users.noreply.github.com>
2024-04-04 11:44:02 -0400

    Add and style products in shop

---
+++ src/components/Card.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import Stars from './Stars';
+import styles from '../style/Card.module.css';
+
+function Card({ product }) {
+  return (
+    <div className={styles.card}>
+      <img src={product.image} alt={product.title} />
+      <h2>{product.title}</h2>
+      <div className={styles.rating}>
+        <p>{product.rating.rate}</p>
+        <Stars rating={product.rating.rate} />
+        <p className={styles.ratingCount}>({product.rating.count})</p>
+      </div>
+      <p className={styles.price}>${product.price}</p>
+    </div>
+  );
+}
+
+Card.propTypes = {
+  product: PropTypes.object,
+}
+
+export default Card
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,19 +1,25 @@
+import { useState, useEffect } from 'react';
+import Card from './Card';
 import styles from '../style/Shop.module.css';
 
 export default function Shop() {
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((res) => res.json())
+      .then((json) => setProducts(json));
+  }, []);
+
   return (
-    <div>
-      Nullam quam leo, blandit nec lorem eget, sodales lacinia tellus. Mauris
-      ultrices cursus urna, ac tincidunt nisi laoreet sit amet. Pellentesque
-      vestibulum leo et facilisis convallis. Pellentesque interdum vel odio eget
-      tincidunt. Fusce accumsan turpis sit amet pulvinar sodales. Phasellus at
-      pellentesque ligula. Aenean dapibus justo id ante finibus sagittis. Sed
-      vitae augue ac diam tempus suscipit. Donec dapibus ut felis in pretium.
-      Cras euismod odio laoreet, malesuada quam sed, feugiat justo. Phasellus
-      vel ultricies ipsum. Orci varius natoque penatibus et magnis dis
-      parturient montes, nascetur ridiculus mus. Nulla eu sapien varius dolor
-      scelerisque vehicula. Nunc sit amet quam rhoncus, tempor est vestibulum,
-      sollicitudin neque.
+    <div className={styles.shop}>
+      <div className={styles.cardContainer}>
+        {products ? (
+          products.map((product) => <Card product={product} key={product.id} />)
+        ) : (
+          <h1 className={styles.loading}>Loading...</h1>
+        )}
+      </div>
     </div>
   );
 }
